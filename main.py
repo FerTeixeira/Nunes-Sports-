1fd246060a3762db376ab12ac12f5237448bee8b
FerTeixeira <fer-teixeirandrade@hotmail.com>
2024-08-29 18:49:22 -0300

    Teste mostrando as tabelas

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import mysql.connector
 
+db = mysql.connector.connect(
+    host="localhost", # Especifica o host do banco de dados
+    user="root", # Especifica o nome do usuário do banco de dados
+    password="root",  # Especifica a senha do usuário do banco de dados
+    database="WallJobs" # Nome do banco de dados que está sendo usado
+)
+
+cursor = db.cursor()
+
+# Executar comandos SQL
+cursor.execute("SHOW TABLES")
+
+# Exibir as tabelas
+for table in cursor:
+    print(table)
+
+# Fechar a conexão
+db.close()
